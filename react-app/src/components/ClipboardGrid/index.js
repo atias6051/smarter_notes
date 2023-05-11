cd11a212f8c327d96d6645e22014460bca24674c
atias6051 <atias6051@gmail.com>
2023-05-11 00:19:31 -0400

    added clipboard component

---
+++ react-app/src/components/ClipboardGrid/index.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function ClipboardGrid(){
+    return (
+        <div>
+            <h3>My copy notes</h3>
+        </div>
+    )
+}
